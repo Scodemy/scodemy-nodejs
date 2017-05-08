718fe250744f20b40b4e4c048842df744d2f551e
belchevgb <gabriel.belchev@protonmail.com>
2017-05-08 21:20:38 +0300

    Fixed identation

--- app/middlewares/sanitizer.js
+++ app/middlewares/sanitizer.js
@@ -1,17 +1,17 @@
 const xss = require('xss')
 
 function sanitizeObject(obj) {
-    const keys = Object.keys(obj)
+  const keys = Object.keys(obj)
 
-    for (const key of keys) {
-        if (typeof obj[key] === 'string')
-            obj[key] = xss(obj[key])
-        else if (Array.isArray(obj[key]))
-            for (let index = 0; index !== obj[key].length; index += 1)
-                obj[key][index] = xss(obj[key][index])
-        else if (typeof obj[key] === 'object')
-            sanitizeObject(obj[key])
-    }
+  for (const key of keys) {
+    if (typeof obj[key] === 'string')
+      obj[key] = xss(obj[key])
+    else if (Array.isArray(obj[key]))
+      for (let index = 0; index !== obj[key].length; index += 1)
+        obj[key][index] = xss(obj[key][index])
+    else if (typeof obj[key] === 'object')
+      sanitizeObject(obj[key])
+  }
 }
 
 function sanitize(req, _, next) {
